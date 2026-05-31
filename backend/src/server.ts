06489ebab731f729a659fdbb1e27dd6018f0375c
Angelita Santos <angelitasantos@gmail.com>
2026-05-31 11:15:31 -0300

    feat: adiciona inicialização do servidor #3

---
+++ backend/src/server.ts
@@ -0,0 +1,10 @@
+import app from './app';
+import { env } from './config/env';
+import { SERVER_RUNNING } from './constants/index';
+
+const PORT = env.PORT;
+
+app.listen(PORT, () => {
+    console.log(SERVER_RUNNING);
+    console.log(`Ambiente ... ${env.NODE_ENV}`);
+}); 
