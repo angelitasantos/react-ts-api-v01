0515d7b6208565b33d2f28253ea40d2a6f245ed5
Angelita Santos <angelitasantos@gmail.com>
2026-05-31 10:36:44 -0300

    chore: configura estrutura de constantes #3

---
+++ backend/src/constants/errors.ts
@@ -0,0 +1,3 @@
+export const DB_CONNECTING_ERROR = 'Erro ao conectar com Banco de Dados:';
+export const INTERNAL_SERVER_ERROR = 'Erro interno do servidor.';
+export const RUNNING_ERROR = 'Erro ao executar:';
---
+++ backend/src/constants/messages.ts
@@ -0,0 +1,10 @@
+import { env } from '../config/env';
+
+export const BACKEND_WORKING = 'Backend funcionando 🚀';
+export const SERVER_RUNNING = `Servidor rodando em ${env.HOST}${env.PORT}`;
+
+export const DB_CONNECTED_SUCCESS = 'Banco de Dados conectado com sucesso.';
+
+export const DEFAULT_MESSAGE_SUCCESS = 'Operação realizada com sucesso.';
+export const ROUTE_NOT_FOUND = 'Rota não encontrada.';
+export const RUNNING = 'Executando ';
