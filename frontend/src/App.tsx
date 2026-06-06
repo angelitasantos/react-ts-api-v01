7748d57950b6f9bd63fb744bed39e048576206a8
Angelita Santos <angelitasantos@gmail.com>
2026-06-06 15:00:20 -0300

    refactor: ajusta rotas para layout publico e manager #13

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,6 @@
-import { Link } from 'react-router-dom';
-import { useDocumentTitle } from './hooks/useDocumentTitle';
-import { HOME_PAGE_TITLE } from './constants/index';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
 
-function App() {
-  useDocumentTitle(HOME_PAGE_TITLE);
-  return (
-    <div>
-      <h1>{HOME_PAGE_TITLE}</h1>
-
-      <Link to='/connection-test'>
-        Testar conexão com Backend
-      </Link>
-    </div>
-  );
+export function App() {
+  return <RouterProvider router={router} />;
 }
-
-export default App;
