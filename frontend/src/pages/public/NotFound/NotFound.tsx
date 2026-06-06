9eecbfc65d990112f7e84101c0243a3bab6641d7
Angelita Santos <angelitasantos@gmail.com>
2026-06-05 21:59:20 -0300

    feat: implementa pagina Not Found #13

---
+++ frontend/src/pages/public/NotFound/NotFound.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import './NotFound.css';
+
+export function NotFound() {
+  return (
+    <div className='not-found'>
+      <h1 className='not-found__title'>404</h1>
+
+      <p className='not-found__message'>
+        Página não encontrada.
+      </p>
+
+      <Link
+        to='/'
+        className='not-found__link'
+      >
+        Voltar para Home
+      </Link>
+    </div>
+  );
+}
