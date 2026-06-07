c4d19df2ba33404a76744de40f930127c2c4331b
Angelita Santos <angelitasantos@gmail.com>
2026-06-07 12:12:22 -0300

    feat: adiciona constantes de load erro pagina about #13

--- frontend/src/constants/errors.ts
+++ frontend/src/constants/errors.ts
@@ -1,3 +1,4 @@
 export const CONNECTION_ERROR_MESSAGE = 'Erro ao conectar com o backend.';
 export const REQUEST_ERROR_MESSAGE = 'Erro na requisição.';
 export const HOME_LOAD_ERROR = 'Não foi possível carregar o conteúdo da página home.';
+export const ABOUT_LOAD_ERROR = 'Não foi possível carregar o conteúdo da página about.';
