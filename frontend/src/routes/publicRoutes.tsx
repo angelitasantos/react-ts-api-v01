a926aa6f35e69c7f51266137ca0a1d9010e057ce
Angelita Santos <angelitasantos@gmail.com>
2026-06-07 12:30:38 -0300

    feat: implementa rota para About #13

--- frontend/src/routes/publicRoutes.tsx
+++ frontend/src/routes/publicRoutes.tsx
@@ -1,13 +1,14 @@
 import { PublicLayout } from '../layouts/PublicLayout/PublicLayout';
 import { NotFound } from '../pages/public/NotFound/NotFound';
 import Home from '../modules/web-home/public-pages/Home';
+import About from '../modules/web-home/public-pages/About';
 
 export const publicRoutes = {
   path: '/',
   element: <PublicLayout />,
   children: [
     { index: true, element: <Home /> },
-    { path: 'about', element: <NotFound /> },
+    { path: 'about', element: <About /> },
     { path: 'contact', element: <NotFound /> },
     { path: 'login', element: <NotFound /> },
   ],
