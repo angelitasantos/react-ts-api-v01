692df61510426fb5a503aa4b98ec2dd68866be53
Angelita Santos <angelitasantos@gmail.com>
2026-06-13 18:19:26 -0300

    feat: atualiza constantes: messages e errors #5

--- backend/src/constants/errors.ts
+++ backend/src/constants/errors.ts
@@ -1,3 +1,14 @@
-export const DB_CONNECTING_ERROR = 'Erro ao conectar com Banco de Dados:';
-export const INTERNAL_SERVER_ERROR = 'Erro interno do servidor.';
-export const RUNNING_ERROR = 'Erro ao executar:';
+export const DB_CONNECTING_ERROR = 'Erro ao conectar com Banco de Dados:'
+export const INTERNAL_SERVER_ERROR = 'Erro interno do servidor.'
+export const RUNNING_ERROR = 'Erro ao executar:'
+export const ROUTE_NOT_FOUND = 'Rota não encontrada.'
+
+export const EXPIRED_OR_INVALID_TOKEN = 'Token expirado ou inválido.'
+export const INVALID_TOKEN = 'Token inválido.'
+export const TOKEN_NOT_PROVIDED = 'Token não informado.'
+
+export const ACCESS_DENIED = 'Acesso negado.'
+export const UNAUTHENTICATED_USER = 'Usuário não autenticado.'
+
+export const INACTIVE_USER = 'Usuário inativo.'
+export const INVALID_EMAIL_OR_PASSWORD = 'Email ou senha inválidos.'
