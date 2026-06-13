fbe242e905e57c8bedbd416fe7841deabe98fb4a
Angelita Santos <angelitasantos@gmail.com>
2026-06-13 19:48:21 -0300

    feat: adapta rotas protegidas para uso de Outlet #10

--- frontend/src/routes/managerRoutes.tsx
+++ frontend/src/routes/managerRoutes.tsx
@@ -1,21 +1,20 @@
-import { ManagerLayout } from '../layouts/ManagerLayout/ManagerLayout';
-import { ProtectedRoute } from '../components/ProtectedRoute/ProtectedRoute';
-import { NotFound } from '../pages/public/NotFound/NotFound';
-import { ConnectionTest } from '../pages/manager/ConnectionTest/ConnectionTest';
-
-const isAuthenticated = true;
+import { ManagerLayout } from '../layouts/ManagerLayout/ManagerLayout'
+import { ProtectedRoute } from '../components/ProtectedRoute/ProtectedRoute'
+import { NotFound } from '../pages/public/NotFound/NotFound'
+import { ConnectionTest } from '../pages/manager/ConnectionTest/ConnectionTest'
 
 export const managerRoutes = {
-  path: '/manager',
-  element: (
-    <ProtectedRoute isAuthenticated={isAuthenticated}>
-      <ManagerLayout />
-    </ProtectedRoute>
-  ),
+  element: <ProtectedRoute />,
   children: [
-    { index: true, element: <NotFound /> },
-    { path: 'users', element: <NotFound /> },
-    { path: 'reports', element: <NotFound /> },
-    { path: 'connection-test', element: <ConnectionTest /> },
+    {
+      path: '/manager',
+      element: <ManagerLayout />,
+      children: [
+        { index: true, element: <NotFound /> },
+        { path: 'users', element: <NotFound /> },
+        { path: 'reports', element: <NotFound /> },
+        { path: 'connection-test', element: <ConnectionTest /> },
+      ],
+    },
   ],
-};
+}
