5200fc385c0746ee7ad7d15372a647c7005d57cd
Angelita Santos <angelitasantos@gmail.com>
2026-06-23 07:06:09 -0300

    refactor: ajusta eslint e codeql

--- scripts/start-dev.js
+++ scripts/start-dev.js
@@ -1,10 +1,6 @@
-const { exec } = require('child_process')
+import { exec } from 'node:child_process'
 
-const URLS = [
-  'http://localhost:3001',
-  'http://localhost:5173',
-]
-
+const URL = 'http://localhost:5173'
 const OPEN_DELAY = 5000
 
 function getOpenCommand(url) {
@@ -27,10 +23,7 @@ function openUrl(url) {
   const command = getOpenCommand(url)
 
   if (!command) {
-    console.error(
-      `Sistema operacional não suportado: ${process.platform}`
-    )
-
+    console.error(`Sistema operacional não suportado: ${process.platform}`)
     return
   }
 
@@ -41,7 +34,6 @@ console.log(`Sistema: ${process.platform}`)
 console.log('Aguardando inicialização dos servidores...')
 
 setTimeout(() => {
-  console.log('Abrindo URLs...')
-
-  URLS.forEach(openUrl)
+  console.log('Abrindo aplicação...')
+  openUrl(URL)
 }, OPEN_DELAY)
